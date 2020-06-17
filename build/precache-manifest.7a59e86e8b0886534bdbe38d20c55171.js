4e3e658e841b943859189012bfa49088d7249288
James Gale <jgaleiphone@gmail.com>
2020-06-16 20:25:05 -0400

    project 16

---
+++ build/precache-manifest.7a59e86e8b0886534bdbe38d20c55171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0bbf3ea743aac97d36d600803750f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0025d35746deff51351c",
+    "url": "/static/css/main.5804a1bc.chunk.css"
+  },
+  {
+    "revision": "60e0a83ffa3290b85ac0",
+    "url": "/static/js/2.c1f11b95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c1f11b95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0025d35746deff51351c",
+    "url": "/static/js/main.94d7afc1.chunk.js"
+  },
+  {
+    "revision": "e44ca8050686401cca12",
+    "url": "/static/js/runtime-main.13213274.js"
+  }
+]);
